b0a596cf248e0ac64ba4ffe49f5a73dce5c73ebb
Shiva Shankar <shivachennaboina9@gmail.com>
2026-04-27 10:05:55 +0530

    fix mongo atlas

--- controllers/listing.js
+++ controllers/listing.js
@@ -28,7 +28,7 @@ module.exports.showListing=async (req,res)=>{
 // create
 
 module.exports.createListing = async (req, res, next) => {
-    // console.log(req.file);
+     console.log(req.file);
   try {
     if (!req.user) {
       req.flash("error", "Login required");
